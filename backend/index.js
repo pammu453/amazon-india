8144044187ca031f896825c0c66ac4ea32a66c5a
pammu453 <pramodsavant2001@gmail.com>
2024-07-28 19:19:37 +0530

    Fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,8 @@ app.use('/api/upload', uploadRouter)
 const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
+
+//middlere for not found url and error handler
 app.use(notFound)
 app.use(errorHandler)
 
